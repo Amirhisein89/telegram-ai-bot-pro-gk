0045994b4cae3427e04250db8aadadc9622c98fb
Amirhisein89 <amirhisein89@gmail.com>
2026-06-24 03:47:38 +0330

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ BOT_TOKEN =os.getenv("BOT_TOKEN")
 
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
 
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+GROQ_API_KEY =("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")
 
 SUBSCRIPTION_DAYS = 7
 
